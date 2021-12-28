81d2d5adcf6c4b2b1bda517b92b830454ca36b8c
gakssultang <mirror1236@gmail.com>
2021-12-28 14:29:39 +0900

    gak1

--- FFT128.srcs/sources_1/imports/new/MULT.v
+++ FFT128.srcs/sources_1/imports/new/MULT.v
@@ -37,7 +37,7 @@ module MULT
     parameter N =32
 )(
     input signed [BW-1:0] data_in_re, data_in_im,
-    input [4:0] cnt,
+    input [5:0] cnt,
     output signed[BW-1:0] data_out_re, data_out_im
 );
     
@@ -181,136 +181,266 @@ module MULT
     wire signed [BW+12:0] buf_im; //[23:0]
           
     always@(*) begin
-    	if (N==32) begin
+
+    	if (N==64) begin
             case(cnt)
-                5'd0 : begin
+                6'd0 : begin
                        temp_re <= W_re0;
                        temp_im <= W_im0;
                        end
-                5'd1 : begin
+                6'd1 : begin
                        temp_re <= W_re1;
                        temp_im <= W_im1;
                        end
-                5'd2 : begin
+                6'd2 : begin
                        temp_re <= W_re2;
                        temp_im <= W_im2;
-                       end            
-                5'd3 : begin
+                       end
+                6'd3 : begin
                        temp_re <= W_re3;
                        temp_im <= W_im3;
-                       end                        
-                5'd4 : begin
+                       end
+                6'd4 : begin
                        temp_re <= W_re4;
                        temp_im <= W_im4;
                        end
-                5'd5 : begin
+                6'd5 : begin
                        temp_re <= W_re5;
                        temp_im <= W_im5;
                        end
-                5'd6 : begin
+                6'd6 : begin
                        temp_re <= W_re6;
                        temp_im <= W_im6;
-                       end       
-                5'd7 : begin
+                       end
+                6'd7 : begin
                        temp_re <= W_re7;
                        temp_im <= W_im7;
                        end
-                5'd8 : begin
+                6'd8 : begin
                        temp_re <= W_re8;
                        temp_im <= W_im8;
                        end
-                5'd9 : begin
+                6'd9 : begin
                        temp_re <= W_re9;
                        temp_im <= W_im9;
                        end
-                5'd10 : begin
+                6'd10 : begin
                        temp_re <= W_re10;
                        temp_im <= W_im10;
                        end
-                5'd11 : begin
+                6'd11 : begin
                        temp_re <= W_re11;
                        temp_im <= W_im11;
                        end
-                5'd12 : begin
+                6'd12 : begin
                        temp_re <= W_re12;
                        temp_im <= W_im12;
                        end
-                5'd13 : begin
+                6'd13 : begin
                        temp_re <= W_re13;
                        temp_im <= W_im13;
                        end
-                5'd14 : begin
+                6'd14 : begin
                        temp_re <= W_re14;
                        temp_im <= W_im14;
-                       end       
-                5'd15 : begin
+                       end
+                6'd15 : begin
                        temp_re <= W_re15;
                        temp_im <= W_im15;
                        end
-                5'd16 : begin
+                6'd16 : begin
                        temp_re <= W_re16;
                        temp_im <= W_im16;
                        end
-                5'd17 : begin
+                6'd17 : begin
                        temp_re <= W_re17;
                        temp_im <= W_im17;
                        end
-                5'd18 : begin
+                6'd18 : begin
                        temp_re <= W_re18;
                        temp_im <= W_im18;
                        end
-                5'd19 : begin
+                6'd19 : begin
                        temp_re <= W_re19;
                        temp_im <= W_im19;
                        end
-                5'd20 : begin
+                6'd20 : begin
                        temp_re <= W_re20;
                        temp_im <= W_im20;
                        end
-                5'd21 : begin
+                6'd21 : begin
                        temp_re <= W_re21;
                        temp_im <= W_im21;
                        end
-                5'd22 : begin
+                6'd22 : begin
                        temp_re <= W_re22;
                        temp_im <= W_im22;
                        end
-                5'd23 : begin
+                6'd23 : begin
                        temp_re <= W_re23;
                        temp_im <= W_im23;
                        end
-                5'd24 : begin
+                6'd24 : begin
                        temp_re <= W_re24;
                        temp_im <= W_im24;
                        end
-                5'd25 : begin
+                6'd25 : begin
                        temp_re <= W_re25;
                        temp_im <= W_im25;
                        end
-                5'd26 : begin
+                6'd26 : begin
                        temp_re <= W_re26;
                        temp_im <= W_im26;
                        end
-                5'd27 : begin
+                6'd27 : begin
                        temp_re <= W_re27;
                        temp_im <= W_im27;
                        end
-                5'd28 : begin
+                6'd28 : begin
                        temp_re <= W_re28;
                        temp_im <= W_im28;
                        end
-                5'd29 : begin
+                6'd29 : begin
                        temp_re <= W_re29;
                        temp_im <= W_im29;
                        end
-                5'd30 : begin
+                6'd30 : begin
                        temp_re <= W_re30;
                        temp_im <= W_im30;
                        end
-                5'd31 : begin
+                6'd31 : begin
                        temp_re <= W_re31;
                        temp_im <= W_im31;
-                       end       
+                       end
+                6'd32 : begin
+                       temp_re <= W_re32;
+                       temp_im <= W_im32;
+                       end
+                6'd33 : begin
+                       temp_re <= W_re33;
+                       temp_im <= W_im33;
+                       end
+                6'd34 : begin
+                       temp_re <= W_re34;
+                       temp_im <= W_im34;
+                       end
+                6'd35 : begin
+                       temp_re <= W_re35;
+                       temp_im <= W_im35;
+                       end
+                6'd36 : begin
+                       temp_re <= W_re36;
+                       temp_im <= W_im36;
+                       end
+                6'd37 : begin
+                       temp_re <= W_re37;
+                       temp_im <= W_im37;
+                       end
+                6'd38 : begin
+                       temp_re <= W_re38;
+                       temp_im <= W_im38;
+                       end
+                6'd39 : begin
+                       temp_re <= W_re39;
+                       temp_im <= W_im39;
+                       end
+                6'd40 : begin
+                       temp_re <= W_re40;
+                       temp_im <= W_im40;
+                       end
+                6'd41 : begin
+                       temp_re <= W_re41;
+                       temp_im <= W_im41;
+                       end
+                6'd42 : begin
+                       temp_re <= W_re42;
+                       temp_im <= W_im42;
+                       end
+                6'd43 : begin
+                       temp_re <= W_re43;
+                       temp_im <= W_im43;
+                       end
+                6'd44 : begin
+                       temp_re <= W_re44;
+                       temp_im <= W_im44;
+                       end
+                6'd45 : begin
+                       temp_re <= W_re45;
+                       temp_im <= W_im45;
+                       end
+                6'd46 : begin
+                       temp_re <= W_re46;
+                       temp_im <= W_im46;
+                       end
+                6'd47 : begin
+                       temp_re <= W_re47;
+                       temp_im <= W_im47;
+                       end
+                6'd48 : begin
+                       temp_re <= W_re48;
+                       temp_im <= W_im48;
+                       end
+                6'd49 : begin
+                       temp_re <= W_re49;
+                       temp_im <= W_im49;
+                       end
+                6'd50 : begin
+                       temp_re <= W_re50;
+                       temp_im <= W_im50;
+                       end
+                6'd51 : begin
+                       temp_re <= W_re51;
+                       temp_im <= W_im51;
+                       end
+                6'd52 : begin
+                       temp_re <= W_re52;
+                       temp_im <= W_im52;
+                       end
+                6'd53 : begin
+                       temp_re <= W_re53;
+                       temp_im <= W_im53;
+                       end
+                6'd54 : begin
+                       temp_re <= W_re54;
+                       temp_im <= W_im54;
+                       end
+                6'd55 : begin
+                       temp_re <= W_re55;
+                       temp_im <= W_im55;
+                       end
+                6'd56 : begin
+                       temp_re <= W_re56;
+                       temp_im <= W_im56;
+                       end
+                6'd57 : begin
+                       temp_re <= W_re57;
+                       temp_im <= W_im57;
+                       end
+                6'd58 : begin
+                       temp_re <= W_re58;
+                       temp_im <= W_im58;
+                       end
+                6'd59 : begin
+                       temp_re <= W_re59;
+                       temp_im <= W_im59;
+                       end
+                6'd60 : begin
+                       temp_re <= W_re60;
+                       temp_im <= W_im60;
+                       end
+                6'd61 : begin
+                       temp_re <= W_re61;
+                       temp_im <= W_im61;
+                       end
+                6'd62 : begin
+                       temp_re <= W_re62;
+                       temp_im <= W_im62;
+                       end
+                6'd63 : begin
+                       temp_re <= W_re63;
+                       temp_im <= W_im63;
+                       end
+                              
               endcase
     	end
     	else if (N==16) begin
